a1fdd670a4fcf10d6adef8da225df4bbc6ec73f3
AadityaJain23 <jainaaditya004@gmail.com>
2025-11-17 18:50:44 +0530

    Day 89

--- Day88_Q138.c
+++ Day88_Q138.c
@@ -1,5 +1,16 @@
 /* Q138: Print all enum names and integer values using a loop. */
 
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+RED=0
+YELLOW=1
+GREEN=2
+
+*/
+
 #include<stdio.h>
 
 enum Color { RED, YELLOW, GREEN };
